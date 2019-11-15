97f21d5dd3ed8ac3d1e7604569e4b30e0559a2d1
Michael Trepanier <mtrepanier91@gmail.com>
2019-11-15 16:06:43 -0700

    some minor updates

--- server.js
+++ server.js
@@ -3,16 +3,11 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-app.use(routes);
-
-app.get("*", function (req, res) {
+app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
